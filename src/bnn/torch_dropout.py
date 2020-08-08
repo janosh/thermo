649977e69da72b41f3edec16321755841b9b3252
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-08-08 15:09:15 +0200

    refactor torch_dropout.py

--- src/bnn/torch_dropout.py
+++ src/bnn/torch_dropout.py
@@ -7,61 +7,66 @@ from torch.nn import functional as F
 from torch.utils.data import Dataset
 from torch.utils.tensorboard import SummaryWriter
 
-from data import load_gaultois, normalize, train_test_split
+from data import dropna, load_gaultois, normalize, train_test_split
 from utils import ROOT
 
 
 class GaultoisData(Dataset):
     def __init__(self, test_size=0.1, train=True, target_cols=None, **kwargs):
         super().__init__(**kwargs)
-        features, labels = load_gaultois(target_cols=target_cols)
-        labels = np.log(labels)
 
-        features, labels = train_test_split(
+        features, labels = load_gaultois(target_cols=target_cols)
+        labels, features = dropna(labels.squeeze(), features)
+
+        self.features, self.labels = train_test_split(
             features, labels, train=train, test_size=test_size
         )
 
-        self.features_df, [self.feature_mean, self.feature_std] = normalize(features)
-        self.labels_df, [self.label_mean, self.label_std] = normalize(labels)
+        X, [self.X_mean, self.X_std] = normalize(self.features)
+        y, [self.y_mean, self.y_std] = normalize(self.labels)
 
-        self.features = torch.Tensor(self.features_df.to_numpy())
-        self.labels = torch.Tensor(self.labels_df.to_numpy())
+        self.X = torch.Tensor(X.to_numpy())
+        self.y = torch.Tensor(y.to_numpy())
 
     def __len__(self):
-        return len(self.features)
+        return len(self.X)
 
     def __getitem__(self, idx):
-        return self.features[idx], self.labels[idx]
+        return self.X[idx], self.y[idx]
 
 
-class TorchDropoutModel(nn.Module):
+class TorchDropoutModel(nn.Sequential):
     """
     Constructs a dropout network with aleatoric and/or epistemic uncertainty estimation.
     """
 
     def __init__(
         self,
-        sizes=[146, 50, 25, 10],
+        sizes=[146, 100, 50, 25, 10],
         drop_rates=[0.5, 0.3, 0.3, 0.3],
         activations=["Tanh", "ReLU", "ReLU", "ReLU"],
         uncertainty="aleatoric_epistemic",
         optimizer=None,
         **kwargs,
     ):
-        err_msg = "length mismatch in TorchDropoutModel hyperparams"
-        assert len(sizes) == len(drop_rates) == len(activations), err_msg
+        err_msg = "length mismatch in hypers"
+        assert len(sizes) - 1 == len(drop_rates) == len(activations), err_msg
 
-        err_msg = f"unexpected uncertainty type: {uncertainty}"
-        assert uncertainty in "aleatoric_epistemic", err_msg
-
-        layers = []
-        for n_in, n_out, act, dr in zip(sizes, sizes[1:], activations, drop_rates):
-            layers.extend([nn.Linear(n_in, n_out), nn.Dropout(dr), getattr(nn, act)()])
-
-        final = nn.Linear(sizes[-1], 1 if uncertainty == "epistemic" else 2)
+        valid_uncert = ["aleatoric", "epistemic", "aleatoric_epistemic"]
+        assert uncertainty in valid_uncert, f"unexpected uncertainty: {uncertainty}"
 
         super().__init__(**kwargs)
-        self.net = nn.Sequential(*layers, final)
+        for idx, [n_in, n_out, act, dr] in enumerate(
+            zip(sizes, sizes[1:], activations, drop_rates), 1
+        ):
+            self.add_module(f"{idx}a", nn.Linear(n_in, n_out))
+            self.add_module(f"{idx}b", nn.Dropout(dr))
+            self.add_module(f"{idx}c", getattr(nn, act)())
+
+        self.add_module(
+            "final", nn.Linear(sizes[-1], 1 if uncertainty == "epistemic" else 2)
+        )
+
         self.uncertainty = uncertainty
 
         now = f"{datetime.now():%Y-%m-%d_%H:%M:%S}"
@@ -69,19 +74,20 @@ class TorchDropoutModel(nn.Module):
         self.epoch = 0
         self.optimizer = optimizer or torch.optim.Adam(self.parameters())
 
-    def forward(self, x):
-        return self.net.forward(x).squeeze()
-
-    def fit(self, loader, epochs=100, print_every=10, write_summary=True, **kwargs):
+    def fit(
+        self, loader, epochs=100, print_every=10, write_loss=True, write_params=False
+    ):
         self.train()
 
         if "aleatoric" in self.uncertainty:
             loss_fn = lambda true, pred: robust_mse(true, *pred.T)
-        if "epistemic" == self.uncertainty:
+        else:
             loss_fn = F.mse_loss
 
+        self.epoch += epochs
         print("epoch\t\ttraining loss\n")
-        for epoch in range(epochs + 1):
+        for epoch in range(self.epoch - epochs, self.epoch + 1):
+            loss = 0
 
             for samples, targets in loader:
                 self.optimizer.zero_grad()
@@ -91,28 +97,48 @@ class TorchDropoutModel(nn.Module):
                 self.optimizer.step()
 
             if epoch % print_every == 0:
-                print(f"{epoch}/{epochs}\t\t{loss:.4g}")
+                print(f"{epoch}/{self.epoch}\t\t{loss:.4g}")
 
-            if write_summary:
+            if write_loss:
                 self.writer.add_scalar("loss/train", loss, self.epoch)
-                self.epoch += 1
+            if write_params:
+                self.write_params(epoch)
 
-    def predict(self, samples, n_preds=100):
+    def predict(self, samples, n_preds=100, raw=False):
         # We're not calling self.eval() here to ensure nn.Dropout remains active.
         self.train()
 
-        if "epistemic" in self.uncertainty:
-            preds = torch.stack([self(samples) for _ in range(n_preds)])
+        if self.uncertainty == "aleatoric":
+            y_pred, y_log_var = self(samples).detach().numpy().T
+            y_var = np.exp(y_log_var)
+        else:
+            output = np.squeeze(
+                [self(samples).detach().numpy() for _ in range(n_preds)]
+            )
+            if raw:
+                return output
+            if self.uncertainty == "epistemic":
+                y_pred, y_var = output.mean(0), output.var(0)
+            else:
+                preds, log_vars = np.rollaxis(output, -1)
+                y_pred, y_var_epist = preds.mean(0), preds.var(0)
+                y_var_aleat = np.exp(log_vars).mean(0)
+                # total variance given by sum of aleatoric and epistemic contribution
+                y_var = y_var_epist + y_var_aleat
 
-        elif "aleatoric" in self.uncertainty:
-            preds = self(samples)
-
-        return preds.detach().numpy()
+        return y_pred, y_var
 
     def write_graph(self, loader):
         samples, _ = next(iter(loader))
         self.writer.add_graph(self, samples)
         self.writer.flush()
+
+    def write_params(self, epoch, write_grads=False):
+        for name, child in self.named_children():
+            for kind, param in child.named_parameters():
+                self.writer.add_histogram(f"{name}_{kind}", param, epoch)
+                if write_grads:
+                    self.writer.add_histogram(f"{name}_{kind}_grad", param.grad, epoch)
 
 
 def robust_mse(y_true, y_pred, y_log_var):
--- src/notebooks/aleatoric_vs_epistemic/torch_dropout.py
+++ src/notebooks/aleatoric_vs_epistemic/torch_dropout.py
@@ -9,9 +9,9 @@ plt.rcParams["figure.figsize"] = [12, 8]
 
 
 # %%
-train_set = GaultoisData(target_cols=["rho"])
+train_set = GaultoisData(target_cols=["rho"], train=True)
 test_set = GaultoisData(target_cols=["rho"], train=False)
-y_true = test_set.labels.numpy().squeeze()
+y_true = test_set.y.numpy()
 
 train_loader = DataLoader(dataset=train_set, batch_size=64, shuffle=True)
 
@@ -33,16 +33,12 @@ both_net.write_graph(train_loader)
 
 
 # %%
-both_pred, both_std = both_net.predict(test_set.features).transpose(2, 0, 1)
-# combine aleatoric and epistemic uncertainty contributions in quadrature
-both_total_std = (both_pred.std(0) ** 2 + both_std.mean(0) ** 2) ** 0.5
+both_pred, both_var = both_net.predict(test_set.X)
 
 
 # %%
-print(f"R^2 score: {r2_score(y_true, both_pred.mean(0)):.4g}")
-plot_output(
-    y_true, both_pred.mean(0), both_total_std, title="Aleatoric + Epistemic Dropout"
-)
+print(f"R^2 score: {r2_score(y_true, both_pred):.4g}")
+plot_output(y_true, both_pred, both_var ** 0.5, title="Aleatoric + Epistemic Dropout")
 
 
 # %% [markdown]
@@ -58,14 +54,12 @@ epistemic_net.fit(train_loader)
 
 
 # %%
-epistemic_pred = epistemic_net.predict(test_set.features)
+epistemic_pred, epistemic_var = epistemic_net.predict(test_set.X)
 
 
 # %%
-print(f"R^2 score: {r2_score(y_true, epistemic_pred.mean(0)):.4g}")
-plot_output(
-    y_true, epistemic_pred.mean(0), epistemic_pred.std(0), title="Epistemic Dropout"
-)
+print(f"R^2 score: {r2_score(y_true, epistemic_pred):.4g}")
+plot_output(y_true, epistemic_pred, epistemic_var ** 0.5, title="Epistemic Dropout")
 
 
 # %% [markdown]
@@ -81,7 +75,7 @@ aleatoric_net.fit(train_loader)
 
 
 # %%
-aleatoric_pred, aleatoric_std = aleatoric_net.predict(test_set.features).T
+aleatoric_pred, aleatoric_std = aleatoric_net.predict(test_set.X)
 
 
 # %%
