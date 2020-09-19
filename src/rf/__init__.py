ae405292873cf9c24a92b3a4707e75ca8680fe30
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-09-19 19:49:56 +0200

    rf nb don't pass ununsed test sets to re_predict

--- src/rf/__init__.py
+++ src/rf/__init__.py
@@ -2,6 +2,10 @@ from rf.forest import RandomForestRegressor
 
 
 def rf_predict(X_train, y_train, X_test, y_test=None, **kwargs):
+    """Fit an RF to (X_train, y_train) and predict on X_test.
+    y_test is only there to match the signature of other predict
+    functions and allow use in predict_multiple_labels().
+    """
     uncertainty = kwargs.pop("uncertainty", None)
 
     forest = RandomForestRegressor(random_state=0, **kwargs)
