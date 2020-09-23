39ba1637b2a307843396d78326383be327fa7f4c
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-09-23 15:47:24 +0200

    vectorize err_decay plot, add random exclusion of samples curve

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -39,27 +39,41 @@ def show_err_decay_dist(decay_by_std, decay_by_err):
     add_text_box(text)
 
 
-def err_decay(err_name, decay_by_std, decay_by_err, title=None):
+def err_decay(y_test, y_pred, y_std, title=None, n_rand=50):
     """Plot for assessing the quality of uncertainty estimates. If a model's
     uncertainty is well calibrated, i.e. strongly correlated with its error,
     removing the most uncertain predictions should make the mean error decay
     similarly to how it decays when removing the predictions of largest error
 
     Args:
-        err_name (str): usually MAE or MSE
         title (str, optional): plot title
     """
-    countdown = range(len(decay_by_std), 0, -1)
+    abs_err = np.abs(y_test - y_pred)
+    y_std_sort = np.argsort(y_std)  # indices that sort y_std in ascending uncertainty
+    n_inc = range(1, len(abs_err) + 1)  # numbers of samples in cumsum()
+
+    decay_by_std = abs_err[y_std_sort].cumsum() / n_inc
+    decay_by_err = np.sort(abs_err).cumsum() / n_inc
+
+    # error decay for random exclusion of samples
+    ae_tile = np.tile(abs_err, [n_rand, 1])
+    [np.random.shuffle(row) for row in ae_tile]  # shuffle rows of ae_tile in place
+    rand_mean = ae_tile.cumsum(1).mean(0) / n_inc
+    rand_std = ae_tile.cumsum(1).std(0) / n_inc
+
+    countdown = range(len(y_test), 0, -1)
     plt.plot(countdown, decay_by_std)
     plt.plot(countdown, decay_by_err)
+    plt.plot(countdown, rand_mean)
+    plt.fill_between(countdown, rand_mean + rand_std, rand_mean - rand_std, alpha=0.2)
+    plt.ylim([0, decay_by_err[-1] * 1.1])
 
-    plt.gca().invert_xaxis()
     show_err_decay_dist(decay_by_std, decay_by_err)
 
-    # n: Number of excluded points starting with points of largest
-    # error/uncertainty, resp.
+    # n: Number of remaining points in err calculation after discarding the
+    # (len(y_test) - n) most uncertain/hightest-error points
     plt.xlabel("$n$")
-    plt.ylabel(f"$\\epsilon_\\mathrm{{{err_name}}}$")
+    plt.ylabel("$\\epsilon_\\mathrm{MAE}$")
     plt.title(title)
 
     fig = plt.gcf()
@@ -68,8 +82,7 @@ def err_decay(err_name, decay_by_std, decay_by_err, title=None):
 
 
 def add_identity(axis, **line_kwargs):
-    """Add a parity line (y = x) (aka identity) to the provided axis.
-    """
+    """Add a parity line (y = x) (aka identity) to the provided axis."""
     # zorder=0 ensures other plotted data displays on top of line
     default_kwargs = dict(alpha=0.5, zorder=0, linestyle="dashed", color="black")
     (identity,) = axis.plot([], [], **default_kwargs, **line_kwargs)
