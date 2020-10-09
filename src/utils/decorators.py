63e58d90f2d2117d3da5dd9acc68e6fa6a93ea8a
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-10-09 12:49:00 +0100

    add interruptable decorator, refactor CSD3 submission scripts

--- src/utils/decorators.py
+++ src/utils/decorators.py
@@ -1,23 +1,47 @@
-import functools
 import os
+from functools import wraps
 from time import time
+from typing import Callable
 
 import IPython
 from matplotlib import pyplot as plt
 
 
-def with_attr(key, val):
-    def wrapper(func):
+def with_attr(key: str, val):
+    """Adds a key-value pair as attribute to the decorated function."""
+
+    def wrapper(func: Callable):
         setattr(func, key, val)
         return func
 
     return wrapper
 
 
-def timed(func):
+def interruptable(orig_func: Callable = None, handler: Callable = None):
+    """Allows to gracefully abort calls to the decorated function with ctrl + c."""
+
+    def wrapper(func: Callable):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except KeyboardInterrupt:
+                handler() if handler else print(
+                    f"\nDetected KeyboardInterrupt: Aborting call to {func.__name__}"
+                )
+
+        return wrapped_function
+
+    if orig_func:
+        return wrapper(orig_func)
+
+    return wrapper
+
+
+def timed(func: Callable):
     """Measures execution time of decorated functions."""
 
-    @functools.wraps(func)
+    @wraps(func)
     def timed_func(*args, **kwargs):
         start = time()
         result = func(*args, **kwargs)
@@ -27,10 +51,10 @@ def timed(func):
     return timed_func
 
 
-def run_once(func):
+def run_once(func: Callable):
     """ensures the decorated function runs at most once in a session"""
 
-    @functools.wraps(func)
+    @wraps(func)
     def once_running_func(*args, **kwargs):
         if once_running_func.has_run is False:
             once_running_func.has_run = True
@@ -40,12 +64,12 @@ def run_once(func):
     return once_running_func
 
 
-def squeeze(func):
+def squeeze(func: Callable):
     """unpacks single-entry lists from the decorated function's return value"""
 
     isiter = lambda x: isinstance(x, (list, tuple))
 
-    @functools.wraps(func)
+    @wraps(func)
     def squeezed_func(*args, **kwargs):
         result = func(*args, **kwargs)
 
@@ -59,7 +83,14 @@ def squeeze(func):
     return squeezed_func
 
 
-def handle_plot(func):
+def in_ipython():
+    """Returns a boolean indicating whether Python is running in interactive mode.
+    To simulate iPython behavior when using a debugger, simply return True.
+    """
+    return IPython.get_ipython() is not None
+
+
+def handle_plot(func: Callable):
     """Decorator for plotting functions. In a regular script, it saves the plot to a path
     passed as `path` kwarg to the decorated function. If used in interactive Python,
     it just displays the output with plt.show() unless the env variable save_to_disk
@@ -76,7 +107,7 @@ def handle_plot(func):
         plt.close()
         return result
 
-    @functools.wraps(func)
+    @wraps(func)
     def saved_plot(*args, **kwargs):
         if not in_ipython() and "path" in kwargs:
             return savefig(*args, **kwargs)
@@ -96,10 +127,3 @@ def handle_plot(func):
             )
 
     return saved_plot
-
-
-def in_ipython():
-    """Returns a boolean indicating whether Python is running in interactive mode.
-    To simulate iPython behavior when using a debugger, simply return True.
-    """
-    return IPython.get_ipython() is not None
